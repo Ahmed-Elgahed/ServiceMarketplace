f70d27a48e5f090a60c9d7585da675003d6e202d
ahmed <ahmedelgahed365@gmail.com>
2026-03-14 01:39:23 +0200

    Remove BlockBruteForceMiddleware temporarily

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 # ===================================
 
 MIDDLEWARE = [
-    'apps.users.middleware.BlockBruteForceMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
